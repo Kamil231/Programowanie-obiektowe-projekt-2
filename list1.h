d2a5efc82bde5daba070d299828954b7383ed009
Kamil231 <32738708+Kamil231@users.noreply.github.com>
2018-03-27 20:35:34 +0200

    Update list1.h

--- list1.h
+++ list1.h
@@ -21,23 +21,23 @@ public:
 	void delete_position(int pos);							//usuwa element na wybranej pozycji
 	void delete_first();									//usuwa pierwszy element
 	void display();											//wyswietla liste
-	void displayrev();										//wyswietla listeb w odwrotnej kolejnoœci
+	void displayrev();										//wyswietla listeb w odwrotnej kolejnoÂœci
 	int length();											//wyswietla dlugosc listy
-	const list1 list1::operator+(list1 & rhs) const;		//dodawanie list
-	const list1 list1::operator-(list1 & rhs) const;		//odejmowanie list
-	const list1 list1::operator=(const list1 & rhs);		//kopiowanie list
+	const list1 operator+(list1 & rhs) const;		//dodawanie list
+	const list1 operator-(list1 & rhs) const;		//odejmowanie list
+	const list1 operator=(const list1 & rhs);		//kopiowanie list
 	void deleteduplicate();									//usuwa powtarzajace sie elementy
-	const list1 list1::operator+=(list1 & rhs);				//dodaje do listy
-	const list1 list1::operator-=(const list1 & list3);		//odejmuje od listy
+	const list1 operator+=(list1 & rhs);				//dodaje do listy
+	const list1 operator-=(const list1 & list3);		//odejmuje od listy
 	int valuebyindex(int i);								//zwraca wartosc elemtnu w danym miejscu listy
-	const bool list1::operator<(list1 & rhs);				
-	const bool list1::operator<=(list1 & rhs);
-	const bool list1::operator>(list1 & rhs);
-	const bool list1::operator>=(list1 & rhs);
-	const bool list1::operator==(list1 & rhs);
-	const bool list1::operator!=(list1 & rhs);
+	const bool operator<(list1 & rhs);				
+	const bool operator<=(list1 & rhs);
+	const bool operator>(list1 & rhs);
+	const bool operator>=(list1 & rhs);
+	const bool operator==(list1 & rhs);
+	const bool operator!=(list1 & rhs);
 	void delete_value(int value);							//usuwa element o zadanej wartosci
 	void delete_betweenvalues(int od, int doo);				//usuwa element ktorego wartosc miesci sie w zadanym przedziale
-	node* list1::operator[](int n);							//zwraca wskaznik na element na danym miejscu
+	node* operator[](int n);							//zwraca wskaznik na element na danym miejscu
 	node* accessbyvalue(int x);								//zwraca wskaznik na element o danej wartosci
 };
